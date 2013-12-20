56db967f752b85779399574fd7d61d6ee3e7ec5f
Sean Dunn <sedunn@adobe.com>
2013-12-20 16:26:14 -0500

    Better log messages for sync command

--- lib/stalker/commands/rsync.rb
+++ lib/stalker/commands/rsync.rb
@@ -10,6 +10,7 @@ module Stalker
       def process_changes(changes)
         puts "Syncing '#{@source}' to '#{@dest}'"
         `rsync -rltz --executability --filter=':- .gitignore' -e ssh #{@source} #{@dest}`
+        puts "Synced '#{@source}' to '#{@dest}'\n\n"
       end
     end
   end
